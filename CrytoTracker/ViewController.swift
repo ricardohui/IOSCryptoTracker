109764f2992ae3b21e8812791e025d1c78b74056
Ricardo Hui <eehaha@gmail.com>
2019-05-27 06:30:06 +0800

    Initial Commit

---
+++ CrytoTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CrytoTracker
+//
+//  Created by Ricardo Hui on 27/5/2019.
+//  Copyright © 2019 Ricardo Hui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
